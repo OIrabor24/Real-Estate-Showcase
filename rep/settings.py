b718fc3357804c94cbd7dfaa33283cb6785e5a8f
Osahon (Sean) Irabor <osahonirabor@hotmail.com>
2022-04-22 18:07:21 -0700

    added staticfilesdir

--- rep/settings.py
+++ rep/settings.py
@@ -162,9 +162,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [ 
-#     os.path.join(BASE_DIR, 'rep/static')
-# ]
+STATICFILES_DIRS = [ 
+    os.path.join(BASE_DIR, 'rep/static')
+]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
